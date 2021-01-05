a10e3e73dca5031efa15ee2109ccf1e644bcc0d3
Charlie <ymao0011@student.monash.edu>
2021-01-05 19:44:51 +0800

    update

---
+++ precache-manifest.e4307023175524695ee2ee01e4c04cd7.js
@@ -0,0 +1,510 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31eff712602b8c24a176c7202e2daccd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "397665a1a8b1a0f637a6",
+    "url": "/static/css/main.141f9f12.chunk.css"
+  },
+  {
+    "revision": "9e27484c6db5de7ace07",
+    "url": "/static/js/2.e7cdc7d5.chunk.js"
+  },
+  {
+    "revision": "a757442294a7e99edddd64346ac5fe85",
+    "url": "/static/js/2.e7cdc7d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "397665a1a8b1a0f637a6",
+    "url": "/static/js/main.563308cf.chunk.js"
+  },
+  {
+    "revision": "3829752f6ec2913760bb",
+    "url": "/static/js/runtime-main.00abad8d.js"
+  },
+  {
+    "revision": "275233e084d854e2b0e08aedf3dcc982",
+    "url": "/static/media/1.275233e0.jpg"
+  },
+  {
+    "revision": "3d769c296baa226d685f1fd8eaf213e6",
+    "url": "/static/media/1.3d769c29.jpg"
+  },
+  {
+    "revision": "81546cfdbe42a606b1c87a9e809570b8",
+    "url": "/static/media/10.81546cfd.jpg"
+  },
+  {
+    "revision": "820b93274aa7ff67c343b6a35d65118e",
+    "url": "/static/media/10.820b9327.jpg"
+  },
+  {
+    "revision": "a7758c353f06794a007a13d7652fe255",
+    "url": "/static/media/11.a7758c35.jpg"
+  },
+  {
+    "revision": "908bb72b2fe564a0658267f0b30ef791",
+    "url": "/static/media/12.908bb72b.jpg"
+  },
+  {
+    "revision": "59d3d55b20ed78489024a5fc8ab9cf70",
+    "url": "/static/media/123.59d3d55b.jpg"
+  },
+  {
+    "revision": "1142d4e02b9ef1c214c13201d7f7cb9f",
+    "url": "/static/media/2.1142d4e0.jpg"
+  },
+  {
+    "revision": "a18e7b788463306ed634df11bb43438c",
+    "url": "/static/media/2.a18e7b78.jpg"
+  },
+  {
+    "revision": "355832a2c41ba0b2cf6a4a590a9f295e",
+    "url": "/static/media/234.355832a2.jpg"
+  },
+  {
+    "revision": "37434ec66a31e7daf94a315a75821629",
+    "url": "/static/media/3.37434ec6.jpg"
+  },
+  {
+    "revision": "6523b53a849f0658160d162c45899230",
+    "url": "/static/media/3.6523b53a.jpg"
+  },
+  {
+    "revision": "33de89a77f9919f33f99bcfb1c2007af",
+    "url": "/static/media/4.33de89a7.jpg"
+  },
+  {
+    "revision": "87a5c6798026719a75412f1a770840cc",
+    "url": "/static/media/4.87a5c679.jpg"
+  },
+  {
+    "revision": "db0ba9919131fa83fec5f2bfb1894afb",
+    "url": "/static/media/5.db0ba991.jpg"
+  },
+  {
+    "revision": "f1ed95b29d85d3ce9a6c600c014bcb9a",
+    "url": "/static/media/5.f1ed95b2.jpg"
+  },
+  {
+    "revision": "b2289f0cb0e93de21d628f181547d899",
+    "url": "/static/media/6.b2289f0c.jpg"
+  },
+  {
+    "revision": "856b10bbbcf5011078148210964fd969",
+    "url": "/static/media/7.856b10bb.jpg"
+  },
+  {
+    "revision": "c671429f62f3a937c2e39cdf6ba4f59d",
+    "url": "/static/media/8.c671429f.jpg"
+  },
+  {
+    "revision": "e13bca0703555acb10d5c7afbc3112d1",
+    "url": "/static/media/8.e13bca07.jpg"
+  },
+  {
+    "revision": "ee26152c8d07dc724f663bf45a5ee810",
+    "url": "/static/media/9.ee26152c.jpg"
+  },
+  {
+    "revision": "74ff89c979a89c915ae855137918612c",
+    "url": "/static/media/A-lock.74ff89c9.jpg"
+  },
+  {
+    "revision": "ea1909d4fc5ec100a490cfd7caeeb1c6",
+    "url": "/static/media/B-internal door.ea1909d4.jpg"
+  },
+  {
+    "revision": "1455e94aea430f884ee98effe69f231f",
+    "url": "/static/media/Family_1.1455e94a.jpg"
+  },
+  {
+    "revision": "a2867829b172174b9177ad84f26e2320",
+    "url": "/static/media/Kitchen-supplier.a2867829.jpg"
+  },
+  {
+    "revision": "e00c49a0c328a603db2b5c2ebcd9b785",
+    "url": "/static/media/Medical_report.e00c49a0.pdf"
+  },
+  {
+    "revision": "563ee93db763e57f13ba3ad724891731",
+    "url": "/static/media/Q1-1.563ee93d.jpg"
+  },
+  {
+    "revision": "0d9f5e40e36a0c5d70e1ed4c707cf526",
+    "url": "/static/media/Q1-C-alarm.0d9f5e40.jpg"
+  },
+  {
+    "revision": "9b48ae16c76753211fba19eb752b98d8",
+    "url": "/static/media/Q2-1.9b48ae16.jpg"
+  },
+  {
+    "revision": "1ea5c1f168fbe9f891c9e7ef5af62545",
+    "url": "/static/media/VICROADS_Dementia ENG.1ea5c1f1.pdf"
+  },
+  {
+    "revision": "5ab23bbcdd10233db86291241a7ea97d",
+    "url": "/static/media/WechatIMG495.5ab23bbc.png"
+  },
+  {
+    "revision": "39d9f0508d74257a01caaba8a5beb283",
+    "url": "/static/media/australia.39d9f050.png"
+  },
+  {
+    "revision": "4a72f860a4b69667b9e7497d42898d93",
+    "url": "/static/media/background.4a72f860.jpg"
+  },
+  {
+    "revision": "1f5f9ec545a55fb1276471859c51bd83",
+    "url": "/static/media/barthel.1f5f9ec5.jpg"
+  },
+  {
+    "revision": "ddae8fe0c30017ca791c19d88662de27",
+    "url": "/static/media/bathroom.ddae8fe0.jpg"
+  },
+  {
+    "revision": "51aed43daa3b3941840121745ed91793",
+    "url": "/static/media/bed.51aed43d.jpg"
+  },
+  {
+    "revision": "80a85f9c6ab62eb420b77cba8c992de4",
+    "url": "/static/media/bedroom.80a85f9c.jpg"
+  },
+  {
+    "revision": "1426d3cb21a704fac9ac354a8d8f3f74",
+    "url": "/static/media/behaviour_1.1426d3cb.jpg"
+  },
+  {
+    "revision": "59b3fbd82b0bb248c46832141b44dbeb",
+    "url": "/static/media/behaviour_2.59b3fbd8.jpg"
+  },
+  {
+    "revision": "0ca6b655dfc855f25ca38ada30a27f39",
+    "url": "/static/media/cabinet.0ca6b655.jpg"
+  },
+  {
+    "revision": "a185827508bf351998e54d9b125c4e8f",
+    "url": "/static/media/card-1.a1858275.jpg"
+  },
+  {
+    "revision": "abaf1e10e675728e7e745f51515cf2af",
+    "url": "/static/media/card-2.abaf1e10.jpg"
+  },
+  {
+    "revision": "8dc4850a083ce246df410769a57300fb",
+    "url": "/static/media/card3副本.8dc4850a.jpg"
+  },
+  {
+    "revision": "4223d765b29d4bb56125e02ea169f27c",
+    "url": "/static/media/chair.4223d765.jpg"
+  },
+  {
+    "revision": "190894b0a7983287d7bd86b68f88a8d5",
+    "url": "/static/media/check_1.190894b0.jpg"
+  },
+  {
+    "revision": "1440a43a2d534c8b7795430cf660d1a0",
+    "url": "/static/media/check_2.1440a43a.jpg"
+  },
+  {
+    "revision": "69c5d4b649bd2c14ac2d9a6394cc3a65",
+    "url": "/static/media/checklist.69c5d4b6.jpg"
+  },
+  {
+    "revision": "00d7b6d94371a8a31bcfa9f64fd2b8b1",
+    "url": "/static/media/china.00d7b6d9.png"
+  },
+  {
+    "revision": "3c143351016b02e785de68b26d656bad",
+    "url": "/static/media/cord.3c143351.jpg"
+  },
+  {
+    "revision": "0722271e642d205323c3afdeaabec0b3",
+    "url": "/static/media/dinnerwear.0722271e.jpg"
+  },
+  {
+    "revision": "102d0125b75b7bffe7c99d799b976f46",
+    "url": "/static/media/doorway.102d0125.jpg"
+  },
+  {
+    "revision": "07f1ad4b8ff684d5ad6ced7daf6ba7fc",
+    "url": "/static/media/driving checklist-A.07f1ad4b.pdf"
+  },
+  {
+    "revision": "64252fc15f14ed79d2564c37f5954820",
+    "url": "/static/media/driving checklist-B.64252fc1.pdf"
+  },
+  {
+    "revision": "24910d9440a06babd9f156e64b1a547c",
+    "url": "/static/media/early_1.24910d94.jpg"
+  },
+  {
+    "revision": "5c60c884634fa7d4e7039e88436d3d40",
+    "url": "/static/media/evaluate-1.5c60c884.jpg"
+  },
+  {
+    "revision": "5da76126e048326c38f2c0a1ab453842",
+    "url": "/static/media/evaluate_2.5da76126.jpg"
+  },
+  {
+    "revision": "95d8f90d524785d4d33ce7baeaace7df",
+    "url": "/static/media/family_2.95d8f90d.jpg"
+  },
+  {
+    "revision": "d45b5cd6c8a11dadc479fe442e022796",
+    "url": "/static/media/fan.d45b5cd6.jpg"
+  },
+  {
+    "revision": "babc98a0b1ced8d0939413f3c806f2c7",
+    "url": "/static/media/garden.babc98a0.jpg"
+  },
+  {
+    "revision": "3fb9e68d09090f4c26a5dfadd18b5c22",
+    "url": "/static/media/grab bar.3fb9e68d.jpg"
+  },
+  {
+    "revision": "3948b2c0e49c9406521954feaca81598",
+    "url": "/static/media/heater.3948b2c0.jpg"
+  },
+  {
+    "revision": "189e150d83e914628b25dde27c773151",
+    "url": "/static/media/home checklist title.189e150d.jpg"
+  },
+  {
+    "revision": "6383c2a947aab53e486118a7006c6a62",
+    "url": "/static/media/home checklist.6383c2a9.pdf"
+  },
+  {
+    "revision": "0a0a64eb80af26aeb91889b5cdf4adfd",
+    "url": "/static/media/home intro.0a0a64eb.jpg"
+  },
+  {
+    "revision": "7471ae5cca2807b9ff75f5107b38b699",
+    "url": "/static/media/home.7471ae5c.jpg"
+  },
+  {
+    "revision": "c331c39c3a9c628d9483dad956db59d2",
+    "url": "/static/media/hometest.c331c39c.jpg"
+  },
+  {
+    "revision": "786fe8676ce36ea64d29e59c114f5b40",
+    "url": "/static/media/indoor.786fe867.jpg"
+  },
+  {
+    "revision": "2d68248634a3e7e5c3fe14d877dcac57",
+    "url": "/static/media/intro.2d682486.png"
+  },
+  {
+    "revision": "803f70565a5749f6a0bf61746fab66b5",
+    "url": "/static/media/introduction.803f7056.jpg"
+  },
+  {
+    "revision": "cc79b1867397b8aec17dccce31e6fbef",
+    "url": "/static/media/italy.cc79b186.png"
+  },
+  {
+    "revision": "fc17569b122a4a632b417922c50370b3",
+    "url": "/static/media/kitchen.fc17569b.jpg"
+  },
+  {
+    "revision": "05ae81a8f2e94c4d5e326e46503ecbd7",
+    "url": "/static/media/light.05ae81a8.jpg"
+  },
+  {
+    "revision": "06fde1d748c5a74b2f704e6ebf23239e",
+    "url": "/static/media/living room.06fde1d7.jpg"
+  },
+  {
+    "revision": "beeef4913557dd806ea20190d26f708e",
+    "url": "/static/media/logo_2.beeef491.png"
+  },
+  {
+    "revision": "d00f5fd25c79140ac4fe106c1df49a5d",
+    "url": "/static/media/mover.d00f5fd2.jpg"
+  },
+  {
+    "revision": "6292866f99f8a2a41c37b683fdb7c182",
+    "url": "/static/media/outdoor.6292866f.jpg"
+  },
+  {
+    "revision": "fcd2e658ed2b21b1693feb1dd0eb1042",
+    "url": "/static/media/outdoor.fcd2e658.jpg"
+  },
+  {
+    "revision": "c140a50915a7feff152395a0146f753b",
+    "url": "/static/media/pathway.c140a509.jpg"
+  },
+  {
+    "revision": "46926cc9aee41048f263baa469a1c0a0",
+    "url": "/static/media/pest.46926cc9.jpg"
+  },
+  {
+    "revision": "54b3fe721d42d2f1127bfec85d96e1e3",
+    "url": "/static/media/product.54b3fe72.jpg"
+  },
+  {
+    "revision": "17b65da774c62588525ec028f75aff22",
+    "url": "/static/media/q1-2.17b65da7.jpg"
+  },
+  {
+    "revision": "a9c300730522a3180e6f3ad69818ce07",
+    "url": "/static/media/q2-2.a9c30073.jpg"
+  },
+  {
+    "revision": "5184f564939aecb9274a56daeb1b9bb8",
+    "url": "/static/media/q2-3.5184f564.jpg"
+  },
+  {
+    "revision": "bc122f956f30a6ef7bc5b6a4ebcbd606",
+    "url": "/static/media/q2-4.bc122f95.jpg"
+  },
+  {
+    "revision": "59fbf4b141026fe95dbb4850c777ea03",
+    "url": "/static/media/q3-1.59fbf4b1.jpg"
+  },
+  {
+    "revision": "15efd7a6d13f267d09f36287701a0de0",
+    "url": "/static/media/q3-2.15efd7a6.jpg"
+  },
+  {
+    "revision": "7d78836a8ce05315f4d339872a6fd5be",
+    "url": "/static/media/q3-3.7d78836a.jpg"
+  },
+  {
+    "revision": "ea5026df02ef4874486998f166d8215f",
+    "url": "/static/media/re-card-1.ea5026df.jpg"
+  },
+  {
+    "revision": "f11d3215017637afab550d3652476f9a",
+    "url": "/static/media/re-card-2.f11d3215.jpg"
+  },
+  {
+    "revision": "2b44fc49fba2a912255a45ed232673af",
+    "url": "/static/media/re-card-3.2b44fc49.jpg"
+  },
+  {
+    "revision": "dc6ef43e49bfba32d09d1f3ec0d20483",
+    "url": "/static/media/reason-need.dc6ef43e.jpg"
+  },
+  {
+    "revision": "98275c6bd4b1a14ca8b7e96a5b69facd",
+    "url": "/static/media/reason-routine.98275c6b.jpg"
+  },
+  {
+    "revision": "9ee6c0945ac6d465ba04ae137f7c3642",
+    "url": "/static/media/reason-search.9ee6c094.jpg"
+  },
+  {
+    "revision": "dae2cd7de72c3409607609b546cc1400",
+    "url": "/static/media/reason-stress.dae2cd7d.jpg"
+  },
+  {
+    "revision": "794e3ba8af9df48c4334473fb52cbf07",
+    "url": "/static/media/rug.794e3ba8.jpg"
+  },
+  {
+    "revision": "930519107a22d0311a26d921598e5209",
+    "url": "/static/media/shower.93051910.jpg"
+  },
+  {
+    "revision": "b6e3ae07a4991bf9bc58c3939da0ab82",
+    "url": "/static/media/small appliance.b6e3ae07.jpg"
+  },
+  {
+    "revision": "4adafb9d272f228a0dff8e2af704f578",
+    "url": "/static/media/smoke alarm.4adafb9d.jpg"
+  },
+  {
+    "revision": "eef940de6292120e59967c89fe851056",
+    "url": "/static/media/stair.eef940de.jpg"
+  },
+  {
+    "revision": "d6b906b4efa81a00ef54277881fb1c4f",
+    "url": "/static/media/stop.d6b906b4.jpg"
+  },
+  {
+    "revision": "95b5dc8df08cf55a70c12f3da20a179c",
+    "url": "/static/media/storage.95b5dc8d.jpg"
+  },
+  {
+    "revision": "0b78dc2bd382846466a9e0bb5dbccbe1",
+    "url": "/static/media/tip-1.0b78dc2b.jpg"
+  },
+  {
+    "revision": "e728da1d8342046ed2d7e54ba7d6430d",
+    "url": "/static/media/tip2.e728da1d.jpg"
+  },
+  {
+    "revision": "0f9e86675859000e6984e75785ed5298",
+    "url": "/static/media/tip3.0f9e8667.jpg"
+  },
+  {
+    "revision": "29c2359359781fcded2ccca621aacb88",
+    "url": "/static/media/tips_contact.29c23593.jpg"
+  },
+  {
+    "revision": "9d0f384a1c6c4f20bdca59391028dd5f",
+    "url": "/static/media/tips_lock.9d0f384a.jpg"
+  },
+  {
+    "revision": "a1a87049f6b5d51553d49a5e46804f97",
+    "url": "/static/media/tips_temp.a1a87049.jpg"
+  },
+  {
+    "revision": "351adff6ee8e8aa45a33a61fb0fce4e5",
+    "url": "/static/media/title.351adff6.jpg"
+  },
+  {
+    "revision": "6ed25b7de6c7ce95a0c80e3d77b39faa",
+    "url": "/static/media/title.6ed25b7d.jpg"
+  },
+  {
+    "revision": "95d5c06bc580cf06602b5e412d63ad44",
+    "url": "/static/media/toaster.95d5c06b.jpg"
+  },
+  {
+    "revision": "a775198d1a1d3e3927bb80b9dd150237",
+    "url": "/static/media/toliet.a775198d.jpg"
+  },
+  {
+    "revision": "e80eb7ce9262da66e1cf483945b8dc30",
+    "url": "/static/media/towel rack.e80eb7ce.jpg"
+  },
+  {
+    "revision": "0fd33949c93f53bf0c187a8563ac0f85",
+    "url": "/static/media/united-arab-emirates.0fd33949.png"
+  },
+  {
+    "revision": "dbcac5f7959018e5a43d962a39049320",
+    "url": "/static/media/vacuum-cleaner.dbcac5f7.jpg"
+  },
+  {
+    "revision": "3fbdceb5c4ccebda9bb3da67a5ca5367",
+    "url": "/static/media/vicroad arabic.3fbdceb5.pdf"
+  },
+  {
+    "revision": "761a4170d4273dc60abe92b1ba4c013a",
+    "url": "/static/media/vicroad chinese.761a4170.pdf"
+  },
+  {
+    "revision": "39cdfadcbe1d6bd1871e94d926b484cc",
+    "url": "/static/media/vicroad italian.39cdfadc.pdf"
+  },
+  {
+    "revision": "0909e9bf064da9e2d9d28ae90fe27e19",
+    "url": "/static/media/vicroad vietnamese.0909e9bf.pdf"
+  },
+  {
+    "revision": "fe6804314ec86ea9da41329cfd63cea8",
+    "url": "/static/media/vietnam.fe680431.png"
+  },
+  {
+    "revision": "fce05f2b6e4af869c82e2ebf47be33f3",
+    "url": "/static/media/wood floor.fce05f2b.jpg"
+  },
+  {
+    "revision": "82ade78654f1038b437f01227720e257",
+    "url": "/static/media/yard.82ade786.jpg"
+  }
+]);
